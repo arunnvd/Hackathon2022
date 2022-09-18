e86f98774dad4810fa655437241fc0bd77368c02
arunnvd <38983682+arunnvd@users.noreply.github.com>
2022-09-18 14:10:17 +0530

    test comitt

--- qr_reader.py
+++ qr_reader.py
@@ -1,3 +1,4 @@
+from multiprocessing.sharedctypes import Value
 import cv2
 from pyzbar.pyzbar import decode
 import json
@@ -13,7 +14,7 @@ firebase_admin.initialize_app(cred, {
 })
 
 #img = cv2.imread('qrcode.png')
-
+""" 
 cam = cv2.VideoCapture(0)
 cv2.namedWindow('temp')
 
@@ -39,6 +40,52 @@ cv2.destroyAllWindows()
 
 tablet_dict = json.loads(barcode[0].data)
 print(tablet_dict['DOM'])
+ """
 
-ref = db.reference('/')
-ref.set(tablet_dict)
+test_json = '''
+{
+    "id" : "dolo_750",  
+    "details" : {
+    "dispName" : "DOLO 550",
+    "DOM" : "01-08-2022",
+    "DOE" : "01-01-2023",
+    "quantity": 20,
+
+    "usage" : {
+        "freq": {},
+        "beforeFood" : false,
+        "period": {"val":1,"unit":"DAY"}
+    },
+    "prescription" : {
+        "doctor":{"id":"D_123","name" : "Dr.John", "specialization" : "General Medicine", "contact" : "09986954699"},
+        "pharma" : {"id":"P_123","name" : "Apollo pharma", "contact" : "09986954699"}
+    }}
+}
+'''
+weight_from_sensor = '''
+{   
+    "value" : 18,
+    "unit" : "gram",
+    "weight_reduction" : [19],
+    "low_weight" : false
+}
+'''
+
+tablet_dict = json.loads(test_json)
+weight_dict = json.loads(weight_from_sensor)
+ref = db.reference(tablet_dict['id'])
+if(ref.get()) :
+    print("medicine already exisist - Update details", ref.child('weight').get())
+    weight_reduction = ref.child('weight').get()['weight_reduction']
+    weight_reduction.append(weight_dict['value'])
+    ref.child('weight').update({
+        'value': weight_dict['value'],
+        "weight_reduction" : weight_reduction,
+    })
+
+else :
+    ref.set(tablet_dict['details'])
+    ref.child('weight').set(weight_dict)
+
+#print(ref.get())
+
